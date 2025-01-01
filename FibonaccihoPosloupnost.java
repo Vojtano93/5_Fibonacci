d79858e9c48361e0bd079c996ca98418bd3c925a
Vojtano93 <189370568+Vojtano93@users.noreply.github.com>
2025-01-01 14:14:51 +0100

    Add files via upload

---
+++ FibonaccihoPosloupnost.java
@@ -0,0 +1,22 @@
+public class FibonaccihoPosloupnost {
+
+    public static void main(String[] args) {
+        System.out.println("Fibonacciho posloupnost");
+
+        // Hodnota vstupu
+        int n = 1000000000;
+
+        // Zobrazení výsledku Fibonacciho čísla
+        System.out.println("Fibonacciho číslo je: " + fibonacci(n));
+    }
+
+    // Rekurzivní metoda pro výpočet Fibonacciho čísla
+    public static int fibonacci(int n) {
+        if (n <= 1) {
+            return n;
+        }
+
+        // Rekurzivní výpočet: F(n) = F(n-1) + F(n-2)
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
